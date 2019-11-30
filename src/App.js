a511fce274b8628ebded7208ab8659a7f357ecc0
tiagofsanchez <tiagofsanchez@gmail.com>
2019-11-30 06:04:59 +0000

    first one

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Users from './users';
+
+
+/*
+This exercise will help you put together and practice all of the concepts you've
+learned thus far. It will also help you form a strong foundational knowledge of
+React and prepare you for your first project.
+
+The instructions for this project are located in the `instructions.md` file.
+*/
+
+class App extends Component {
+  
+  state = {
+  	users: []
+  }
+  
+
+storeUserHandler = (user) => {
+	this.setSate( prevState => ({users:[...prevState.users,user] }) )
+
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+    	<Users onStoreUser={this.storeUserHandler}/>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
